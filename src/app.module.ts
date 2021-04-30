bb7a1b99cbf92b27809f71affe966e7c1b660eff
jamborghini <tyserract@gmail.com>
2021-04-30 04:45:47 +0300

    remove deleted modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { WebsocketModule } from 'src/module/websocket/websocket.module';
 
 @Module({
   imports: [WebsocketModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
