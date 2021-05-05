b6b3fb0c5ef2843cf16f41e770fa69045f9d85aa
jamborghini <tyserract@gmail.com>
2021-05-05 19:25:26 +0300

    create user model

--- src/module/user/model/user.ts
+++ src/module/user/model/user.ts
@@ -1,20 +1,31 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-import { Column, Entity } from 'typeorm';
+import { BeforeInsert, Column, Entity } from 'typeorm';
 import { Substructure } from 'src/module/shared/model/substructure';
 import { Role } from 'src/module/user/model/enum/role';
+import { hash } from 'bcrypt';
 
 @ObjectType()
 @Entity()
 export class User extends Substructure {
   @Field()
   @Column()
-  email: string;
+  username: string;
 
   @Field()
   @Column()
   password: string;
 
   @Field(() => Role)
-  @Column({ type: 'enum', enum: Role })
+  @Column({ type: 'enum', enum: Role, default: Role.User })
   role: Role;
+
+  toJSON() {
+    const { password, ...result } = this;
+    return result;
+  }
+
+  @BeforeInsert()
+  private async hashPassword() {
+    this.password = await hash(this.password, 14);
+  }
 }
