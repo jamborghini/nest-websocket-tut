1e9632e24953a52bd942cf3afb5d75318b43f6fa
jamborghini <tyserract@gmail.com>
2021-05-05 19:26:26 +0300

    Revert "create user model"
    This reverts commit b6b3fb0c

--- src/module/user/model/user.ts
+++ src/module/user/model/user.ts
@@ -1,31 +1,20 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-import { BeforeInsert, Column, Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import { Substructure } from 'src/module/shared/model/substructure';
 import { Role } from 'src/module/user/model/enum/role';
-import { hash } from 'bcrypt';
 
 @ObjectType()
 @Entity()
 export class User extends Substructure {
   @Field()
   @Column()
-  username: string;
+  email: string;
 
   @Field()
   @Column()
   password: string;
 
   @Field(() => Role)
-  @Column({ type: 'enum', enum: Role, default: Role.User })
+  @Column({ type: 'enum', enum: Role })
   role: Role;
-
-  toJSON() {
-    const { password, ...result } = this;
-    return result;
-  }
-
-  @BeforeInsert()
-  private async hashPassword() {
-    this.password = await hash(this.password, 14);
-  }
 }
