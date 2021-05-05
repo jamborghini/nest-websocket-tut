3516152337a00234756fef5ec2d89b58efb91738
jamborghini <tyserract@gmail.com>
2021-05-05 19:25:11 +0300

    create payload prop validator

---
+++ src/module/decorator/param/payload.ts
@@ -0,0 +1,8 @@
+import { Args, ArgsOptions } from '@nestjs/graphql';
+
+export function Payload(
+  property: string,
+  options?: ArgsOptions,
+): ParameterDecorator {
+  return Args(property ?? 'payload', options);
+}
