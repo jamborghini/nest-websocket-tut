18d57683cec52eceaaab0a0114fea5069819766b
jamborghini <tyserract@gmail.com>
2021-05-05 19:24:43 +0300

    create id prop validator

---
+++ src/module/decorator/param/id.ts
@@ -0,0 +1,8 @@
+import { Args, ArgsOptions } from '@nestjs/graphql';
+
+export function Id(
+  property: string,
+  options?: ArgsOptions,
+): ParameterDecorator {
+  return Args(property ?? 'id', options);
+}
