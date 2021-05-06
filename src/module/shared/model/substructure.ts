e83fb1b3c2a4cfe459b29a18b735150bfde8a77c
jamborghini <tyserract@gmail.com>
2021-05-06 12:49:14 +0300

    error handling

--- src/module/shared/model/substructure.ts
+++ src/module/shared/model/substructure.ts
@@ -3,11 +3,16 @@ import {
   BaseEntity,
   CreateDateColumn,
   DeleteDateColumn,
+  EntityNotFoundError,
+  FindConditions,
+  FindOneOptions,
+  ObjectType as ObjectType$,
   PrimaryGeneratedColumn,
   SaveOptions,
   UpdateDateColumn,
 } from 'typeorm';
-import { ConflictException } from '@nestjs/common';
+import { ConflictException, NotFoundException } from '@nestjs/common';
+import { ObjectID } from 'typeorm/driver/mongodb/typings';
 
 @ObjectType()
 export class Substructure extends BaseEntity {
@@ -31,6 +36,37 @@ export class Substructure extends BaseEntity {
     return super.save(options).catch((error) => {
       if (error?.code === '23505') {
         throw new ConflictException();
+      }
+
+      throw error;
+    });
+  }
+
+  static findOneOrFail<T extends BaseEntity>(
+    this: ObjectType$<T>,
+    id?: string | number | Date | ObjectID,
+    options?: FindOneOptions<T>,
+  ): Promise<T>;
+
+  static findOneOrFail<T extends BaseEntity>(
+    this: ObjectType$<T>,
+    options?: FindOneOptions<T>,
+  ): Promise<T>;
+
+  static findOneOrFail<T extends BaseEntity>(
+    this: ObjectType$<T>,
+    conditions?: FindConditions<T>,
+    options?: FindOneOptions<T>,
+  ): Promise<T>;
+
+  static findOneOrFail(...args) {
+    if (args[0] instanceof Substructure) {
+      args[0] = args[0].id;
+    }
+
+    return super.findOneOrFail(...args).catch((error) => {
+      if (error instanceof EntityNotFoundError) {
+        return new NotFoundException(this.name);
       }
 
       throw error;
