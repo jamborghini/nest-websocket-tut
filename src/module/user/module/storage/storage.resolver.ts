c965ad9625560a02126dc6c1a9effa07b3e87b2f
jamborghini <tyserract@gmail.com>
2021-05-06 12:07:34 +0300

    add mutation & queries

--- src/module/user/module/storage/storage.resolver.ts
+++ src/module/user/module/storage/storage.resolver.ts
@@ -1,4 +1,46 @@
-import { Resolver } from '@nestjs/graphql';
+import {
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
+import { Storage } from 'src/module/user/module/storage/model/storage';
+import { User } from 'src/module/user/model/user';
+import { Id } from 'src/module/decorator/param/id';
+import { Payload } from 'src/module/decorator/param/payload';
+import { CreateStorage } from 'src/module/user/module/storage/input/create-storage';
+import { plainToClass, plainToClassFromExist } from 'class-transformer';
+import { UpdateStorage } from 'src/module/user/module/storage/input/update-storage';
+import { DeleteStorage } from 'src/module/user/module/storage/input/delete-storage';
 
-@Resolver()
-export class StorageResolver {}
+@Resolver(() => Storage)
+export class StorageResolver {
+  @Query(() => Storage)
+  async storage(@Id() id: number): Promise<Storage> {
+    return Storage.findOneOrFail(id);
+  }
+
+  @Mutation(() => Storage)
+  async createStorage(@Payload() payload: CreateStorage): Promise<Storage> {
+    payload.user = await User.findOneOrFail(payload.user);
+    return plainToClass(Storage, payload).save();
+  }
+
+  @Mutation(() => Storage)
+  async updateStorage(@Payload() payload: UpdateStorage): Promise<Storage> {
+    const storage = await Storage.findOneOrFail(payload.id);
+    return plainToClassFromExist(storage, payload).save();
+  }
+
+  @Mutation(() => Storage)
+  async deleteStorage(@Payload() payload: DeleteStorage): Promise<Storage> {
+    const storage = await Storage.findOneOrFail(payload.id);
+    return storage.softRemove();
+  }
+
+  @ResolveField(() => User)
+  async user(@Parent() user: User): Promise<User> {
+    return User.findOneOrFail(user.storage);
+  }
+}
--- src/module/user/user.resolver.ts
+++ src/module/user/user.resolver.ts
@@ -1,10 +1,52 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import {
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
 import { User } from 'src/module/user/model/user';
+import { Id } from 'src/module/decorator/param/id';
+import { Payload } from 'src/module/decorator/param/payload';
+import { CreateUser } from 'src/module/user/input/create-user';
+import { plainToClass, plainToClassFromExist } from 'class-transformer';
+import { UpdateUser } from 'src/module/user/input/update-user';
+import { DeleteUser } from 'src/module/user/input/delete-user';
+import { Storage } from 'src/module/user/module/storage/model/storage';
 
 @Resolver(() => User)
 export class UserResolver {
   @Query(() => User)
-  async user(@Args('id') id: number): Promise<User> {
+  async user(@Id() id: number): Promise<User> {
     return User.findOneOrFail(id);
+  }
+
+  @Query(() => [User])
+  async users(): Promise<User[]> {
+    return User.find();
+  }
+
+  @Mutation(() => User)
+  async createUser(@Payload() payload: CreateUser): Promise<User> {
+    return plainToClass(User, payload).save();
+  }
+
+  @Mutation(() => User)
+  async updateUser(@Payload() payload: UpdateUser): Promise<User> {
+    const user = await User.findOneOrFail(payload.id);
+    return plainToClassFromExist(user, payload).save();
+  }
+
+  @Mutation(() => User)
+  async deleteUser(@Payload() payload: DeleteUser): Promise<User> {
+    const user = await User.findOneOrFail(payload.id, {
+      loadRelationIds: true,
+    });
+    return user.softRemove();
+  }
+
+  @ResolveField(() => Storage)
+  async storage(@Parent() user: User): Promise<Storage> {
+    return Storage.findOne({ user });
   }
 }
