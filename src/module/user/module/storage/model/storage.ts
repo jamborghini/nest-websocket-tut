5712e667d9ad914843d36f23f8490f66b0adc5c6
jamborghini <tyserract@gmail.com>
2021-05-06 12:05:13 +0300

    correct relation with user

--- src/module/user/module/storage/model/storage.ts
+++ src/module/user/module/storage/model/storage.ts
@@ -1,6 +1,6 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { Substructure } from 'src/module/shared/model/substructure';
-import { Column, Entity, OneToOne } from 'typeorm';
+import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
 import { User } from 'src/module/user/model/user';
 
 @ObjectType()
@@ -10,6 +10,8 @@ export class Storage extends Substructure {
   @Column()
   lastMessage: string;
 
+  @Field(() => User)
   @OneToOne(() => User, (user) => user.storage)
+  @JoinColumn()
   user: User;
 }
