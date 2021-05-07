a102abd8ef971faf0ef02e75d2364ee339ec9f57
jamborghini <tyserract@gmail.com>
2021-05-07 10:08:28 +0300

    -

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(4000);
+  await app.listen(4000, '0.0.0.0');
 }
 bootstrap();
--- src/module/websocket/chat/chat.gateway.ts
+++ src/module/websocket/chat/chat.gateway.ts
@@ -1,27 +1,44 @@
 import {
-  MessageBody,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-
-export interface TypingUser {
-  username: string;
-}
+import { ChatEvent } from 'src/module/websocket/chat/enum/chat-event';
+import { OnModuleInit } from '@nestjs/common';
 
 @WebSocketGateway()
-export class ChatGateway {
+export class ChatGateway implements OnModuleInit {
   @WebSocketServer()
   private server: Server;
 
-  @SubscribeMessage('message')
-  handleMessage(@MessageBody() message: Record<any, any>): void {
-    this.server.emit('message', message);
+  onModuleInit(): void {
+    this.server.on('connection', (data) => {
+      this.server.emit(
+        ChatEvent.CLIENT_COUNT,
+        data.conn.server.clientsCount - 1,
+      );
+      data.on('disconnect', () => {
+        this.server.emit(
+          ChatEvent.CLIENT_COUNT,
+          data.conn.server.clientsCount - 1,
+        );
+      });
+    });
   }
 
-  @SubscribeMessage('typing')
-  handleTypingStart(client: Socket, data: TypingUser): void {
-    client.broadcast.emit('typing', data);
+  @SubscribeMessage(ChatEvent.MESSAGE)
+  handleMessage(client: Socket, data: any): void {
+    client.broadcast.emit(ChatEvent.MESSAGE, data);
+  }
+
+  @SubscribeMessage(ChatEvent.READ)
+  handleRead(client: Socket): void {
+    client.broadcast.emit(ChatEvent.READ);
+  }
+
+  @SubscribeMessage(ChatEvent.POKE)
+  handlePoke(client: Socket): void {
+    client.broadcast.emit(ChatEvent.POKE);
   }
 }
