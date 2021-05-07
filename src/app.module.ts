d4404fbca796771c5592491418d69e5b62c4f458
jamborghini <tyserract@gmail.com>
2021-05-07 10:55:36 +0300

    -

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,17 +6,17 @@ import { UserModule } from 'src/module/user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'root',
-      password: 'root',
-      database: 'temp',
-      entities: ['dist/**/model/*.{ts,js}'],
-      synchronize: true,
-      dropSchema: true,
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'root',
+    //   password: 'root',
+    //   database: 'temp',
+    //   entities: ['dist/**/model/*.{ts,js}'],
+    //   synchronize: true,
+    //   dropSchema: true,
+    // }),
     WebsocketModule,
     UserModule,
     GraphQLModule.forRoot({
